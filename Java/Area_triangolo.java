6a07c3a425528fdc19fe0a654505fb6ad5866013
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-02-20 12:26:34 +0100

    Esercizi Java

---
+++ Java/Area_triangolo.java
@@ -0,0 +1,13 @@
+package Develhope;
+
+public class Area_triangolo {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
+      int z=(x*y)/2;
+      
+      System.out.println("Base del triangolo: " + x);
+      System.out.println("Altezza del triangolo: " + y);
+      System.out.println("Area del triangolo: " + z);
+    }
+}
---
+++ Java/CharacterTypes.java
@@ -0,0 +1,16 @@
+package Develhope;
+public class CharacterTypes {
+
+        public static void main(String[] args) {
+            // Dichiarazione di due caratteri
+            char char1 = 'A';
+            char char2 = 'B';
+    
+            // Combinazione dei due caratteri in una stringa senza spazi
+            String combinedString = "" + char1 + char2;
+    
+            // Stampa del risultato
+            System.out.println("La combinazione dei caratteri " + char1 + " e " + char2 + " è: " + combinedString);
+        }
+    }
+
