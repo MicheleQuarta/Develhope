9344d20adc19a86bfc7eae013702003b85b77233
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-03-01 11:06:24 +0100

    Update Auto.java
    spostato main su una classe apparte

--- Java2/Auto.java
+++ Java2/Auto.java
@@ -44,12 +44,5 @@ public class Auto {
     public void setModello(String modello) {
         this.modello = modello;
     }
-    public static void main(String[] args) {
-        Auto auto1 = new Auto(2000, "AB123CD", "Fiat", "Panda");
-        System.out.println("La mia auto ha cilindrata: " + auto1.getCilindrata());
-        System.out.println("La mia auto ha targa: " + auto1.getTarga());
-        System.out.println("La mia auto è della marca: " + auto1.getMarca());
-        System.out.println("Il modello della mia auto è: " + auto1.getModello());
-    }
 }
 
