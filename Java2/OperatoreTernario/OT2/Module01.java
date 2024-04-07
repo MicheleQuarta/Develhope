c8691e4262b0bf8733befd0eff746f2e8a1cae58
MicheleQuarta <160587660+MicheleQuarta@users.noreply.github.com>
2024-04-07 20:50:13 +0200

    Esercizio op ternario 2

---
+++ Java2/OperatoreTernario/OT2/Module01.java
@@ -0,0 +1,29 @@
+package Java2.OperatoreTernario.OT2;
+
+/*
+public class Module01 {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+        String result;
+        if (value.length() >= 10) {
+            result = "Lunghezza maggiore o uguale di 10";
+        } else {
+            result = "Lunghezza minore di 10";
+        }
+        return result;
+    }
+}
+*/
+
+public class Module01 {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+        return (value.length() >= 10) ? "Lunghezza maggiore o uguale di 10" : "Lunghezza minore di 10";
+    }
+}
